6508591628a1b1202296d77a7365bb422b38b4ca
Ahsan <64372074+ahsan1800411@users.noreply.github.com>
2021-06-27 09:49:10 +0500

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import BookList from "./components/BookList";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import AddBook from "./components/AddBook";
+
+// apollo client
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>My Reading List</h1>
+        <BookList />
+        <AddBook />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
